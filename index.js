8ee84b472905ed2c5337e8bd5a9b3ed84f7dddbf
Ben Kwon <arkanumbk@gmail.com>
2025-09-15 13:11:16 -0700

    Add API call code

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 #!/usr/bin/env node
 
+async function getUserActivity(username){
+    const url = `https://api.github.com/users/${username}/events`
+    const headers = {
+        'Accept' : 'application/vnd.github+json',
+        'X-GitHub-Api-Version': '2022-11-28',
+    };
+
+    try{
+        const response = await fetch(url,{
+            method: 'GET',
+            headers: headers
+        });
+        if(!response.ok){
+            throw new Error(`Response status: ${response.status}`, response.statusText)
+        }
+
+        const result = await response.json();
+    } catch(error){
+        console.error(error.message);
+    }
+
+    
+}
+
+const args = process.argv.slice(2);
+
+getUserActivity(args[0]);
