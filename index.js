016c40a9257c2a42ea1e86d0298520fbad0c8cbc
evolentDelight <arkanumbk@gmail.com>
2025-09-17 18:14:52 -0700

    add updated import code

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 #!/usr/bin/env node
+import "dotenv/config";
 
-async function getUserActivity(username){
-    const url = `https://api.github.com/users/${username}/events`
-    const headers = {
-        'Accept' : 'application/vnd.github+json',
-        'X-GitHub-Api-Version': '2022-11-28',
-    };
+async function getUserActivity(username) {
+  const url = `https://api.github.com/users/${username}/events`;
+  const headers = {
+    Accept: "application/vnd.github+json",
+    "X-GitHub-Api-Version": "2022-11-28",
+  };
 
-    try{
-        const response = await fetch(url,{
-            method: 'GET',
-            headers: headers
-        });
-        if(!response.ok){
-            throw new Error(`Response status: ${response.status}`, response.statusText)
-        }
-
-        const result = await response.json();
-    } catch(error){
-        console.error(error.message);
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: headers,
+    });
+    if (!response.ok) {
+      throw new Error(
+        `Response status: ${response.status}`,
+        response.statusText
+      );
     }
 
-    
+    const result = await response.json();
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 const args = process.argv.slice(2);
 
-getUserActivity(args[0]);
+//getUserActivity(args[0]);
