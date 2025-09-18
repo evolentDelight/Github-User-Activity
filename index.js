235aa5279a5d6babbb7d1e8f9c682e110a0d0246
evolentDelight <arkanumbk@gmail.com>
2025-09-17 18:24:14 -0700

    Working fetch API for user public events

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ async function getUserActivity(username) {
   const headers = {
     Accept: "application/vnd.github+json",
     "X-GitHub-Api-Version": "2022-11-28",
+    Authorization: `Bearer ${process.env.access_token}`,
   };
 
   try {
@@ -21,6 +22,7 @@ async function getUserActivity(username) {
     }
 
     const result = await response.json();
+    console.log(result);
   } catch (error) {
     console.error(error.message);
   }
@@ -28,4 +30,4 @@ async function getUserActivity(username) {
 
 const args = process.argv.slice(2);
 
-//getUserActivity(args[0]);
+getUserActivity(args[0]);
