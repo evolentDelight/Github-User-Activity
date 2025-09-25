ab83ce43a4df23f484f349ac9b2a247e993073ad
evolentDelight <arkanumbk@gmail.com>
2025-09-24 18:01:14 -0700

    Added WatchEvent event

--- handleOutputByEvent.js
+++ handleOutputByEvent.js
@@ -1,25 +1,27 @@
-function pushEvent(event){
-  return `Pushed ${event.payload.size} commits to ${event.repo.name}`
+function pushEvent(event) {
+  return `Pushed ${event.payload.size} commits to ${event.repo.name}`;
 }
 
-export default function handleOutputByEvent(events){
+function watchEvent(event) {
+  return `Started watching ${event.repo.name}`;
+}
+
+export default function handleOutputByEvent(events) {
   let outputs = [];
 
   events.forEach((event) => {
     const eventType = event.type;
-    let result = "- ";//Initial console output formatting
+    let result = "- "; //Initial console output formatting
 
     //Handle determining the type of event
-    if(eventType == 'PushEvent') result += pushEvent(event);
+    if (eventType == "PushEvent") result += pushEvent(event);
 
     //Add Date to console output
-    const date = new Date(event.created_at)
-    result += `\t - ${date.toLocaleString()}`
+    const date = new Date(event.created_at);
+    result += `\t - ${date.toLocaleString()}`;
 
-    outputs.push(result)
-  }
+    outputs.push(result);
+  });
 
-  )
-
-  return outputs
+  return outputs;
 }
